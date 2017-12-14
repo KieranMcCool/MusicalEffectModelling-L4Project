e79169d9b75ac7ff7386af3b03ba3c5a21397820
Kieran McCool <kieranmccool332@gmail.com>
2017-12-14 00:41:49 +0000

    updated pipeline

--- Experiments/Pipeline/generate.sh
+++ Experiments/Pipeline/generate.sh
@@ -1,25 +1,36 @@
 rm -rf dataset
+rm -rf model_outputs
 
 mkdir dataset
+mkdir model_outputs
 
 ./main.py -n $1 -s $2
 
-echo $(find dataset -iname \*.wav -exec echo "$(pwd)/{}\n"  \;) "<CONFIG
-FXCHAIN \"$(pwd)/$3\"
-OUTPATH \"$(pwd)/dataset/processed\"
-<RENDERPRESET render 0 0 0 0 3 0>
->" > reaper && /Applications/REAPER64.app/Contents/MacOS/REAPER -newinst -batchconvert reaper
+echo $(find dataset -iname \*.wav -exec echo "$(pwd)/{}\n"  \;) \
+    "<CONFIG
+    FXCHAIN \"$(pwd)/$3\"
+    OUTPATH \"$(pwd)/dataset/processed\"
+    <RENDERPRESET render 0 0 0 0 3 0>
+    >" > reaper \
+&& /Applications/REAPER64.app/Contents/MacOS/REAPER -newinst -batchconvert reaper
 
 find ./dataset/processed/ -name '*.wav' |  while read line ; do
 mv "$line" "$(echo $line | sed 's|/- ||g')";
 done;
 
-echo $(find model_outputs -iname \*.wav -exec echo "$(pwd)/{}\n"  \;) "<CONFIG
-FXCHAIN \"$(pwd)/$3\"
-OUTPATH \"$(pwd)/model_outputs/processed\"
-<RENDERPRESET render 0 0 0 0 3 0>
->" > reaper && /Applications/REAPER64.app/Contents/MacOS/REAPER -newinst -batchconvert reaper
+cp ./testfiles/* ./model_outputs/
+
+echo $(find model_outputs -iname \*.wav -exec echo "$(pwd)/{}\n"  \;) \
+    "<CONFIG
+    FXCHAIN \"$(pwd)/$3\"
+    OUTPATH \"$(pwd)/model_outputs/processed\"
+    <RENDERPRESET render 0 0 0 0 3 0>
+    >" > reaper \
+&& /Applications/REAPER64.app/Contents/MacOS/REAPER -newinst -batchconvert reaper
 
 find ./model_outputs/processed/ -name '*.wav' |  while read line ; do
 mv "$line" "$(echo $line | sed 's|/- ||g')";
 done;
+
+rm reaper
+rm reaper.log
